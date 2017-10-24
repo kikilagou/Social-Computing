e39e789a6055581c9d5da73f359176cff76b5605
Iarina Dafin <itd1e14@soton.ac.uk>
2017-10-24 18:00:42 +0100

    Test: creates a new table, puts some info in, prints a select query and then drops table.

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,62 @@
+import java.io.File;
+import java.sql.*;
+
 /**
  * Created by Iarina Dafin on 17/10/24.
  */
 
-import java.sql.Connection;
-import java.sql.DatabaseMetaData;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
 public class Main {
 
     /**
-     * @author sqlitetutorial.net
-     * Connect to a sample database
+     * author: sqlitetutorial.net
+     * found at: https://sqlite.org/quickstart.html
+     *
+     * Connects to a new database.
      * @param fileName the database file name
      */
-    public static void createNewDatabase(String fileName) {
+    public static void createNewDatabase (String fileName) {
 
-        String url = "jdbc:sqlite:C:\\Users\\Iarina Dafin\\Desktop\\SQLite" + fileName;
+        String url = "jdbc:sqlite:" + System.getProperty("user.dir").replace('/', '\\') + fileName;
 
-        try (Connection conn = DriverManager.getConnection(url)) {
+        try {
+            Connection conn = DriverManager.getConnection(url);
+
             if (conn != null) {
                 DatabaseMetaData meta = conn.getMetaData();
                 System.out.println("The driver name is " + meta.getDriverName());
                 System.out.println("A new database has been created.");
             }
+
+            String db = "jdbc:sqlite:SQLite";
+
+            // SQL statement for creating a new table
+            String sql = "CREATE TABLE test (\n"
+                    + "	key integer PRIMARY KEY,\n"
+                    + "	id int,\n"
+                    + "	name varchar(255)"
+                    + ");";
+
+            Statement stmt = conn.createStatement();
+            // create a new table
+            stmt.execute(sql);
+
+            String sqlinsert = "INSERT INTO test (id, name) VALUES(?,?)";
+
+            PreparedStatement pstmt = conn.prepareStatement(sqlinsert);
+            pstmt.setInt(1, 13);
+            pstmt.setString(2, "name");
+
+            pstmt.executeUpdate();
+
+            String sqlselect = "SELECT * FROM test";
+            ResultSet rs = stmt.executeQuery(sqlselect);
+
+            while (rs.next()) {
+                System.out.println(rs.getInt("id") + rs.getString("name"));
+            }
+
+            String sqldrop = "DROP TABLE IF EXISTS test";
+            stmt.execute(sqldrop);
 
         } catch (SQLException e) {
             System.out.println(e.getMessage());
@@ -33,7 +66,7 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        createNewDatabase("test.db");
+    public static void main (String[] args) {
+        createNewDatabase ("test.db");
     }
 }
