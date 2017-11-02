ee6d03df1b1b4f64b4d4b71084037db1ec768cfd
Iarina Dafin <itd1e14@soton.ac.uk>
2017-11-02 17:59:26 +0000

    A completely unorganised attempt at many things (item-item).

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.io.File;
 import java.sql.*;
+import java.util.HashMap;
 
 /**
  * Created by Iarina Dafin on 17/10/24
@@ -9,6 +10,9 @@ public class Main {
 
     private Connection  connection;
     private Statement statement;
+    private PreparedStatement stat;
+    private float[][] simMatrix = new float[5000][5000];
+    private HashMap<Integer, Float> map = new HashMap<Integer, Float>();
 
     /**
      * credits go to: sqlitetutorial.net
@@ -56,22 +60,6 @@ public class Main {
     }
 
     /**
-     * Example query made on the db.
-     */
-    private void query () {
-        System.out.println("Test query of the first 10 lines of the CSV.");
-        try {
-            ResultSet result = statement.executeQuery("SELECT * FROM database LIMIT 10;");
-
-            while (result.next()) {
-                System.out.println(result.getInt("userID") + " " + result.getInt("itemID") + " " + result.getInt("rating"));
-            }
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
-
-    /**
      * Drops the table and deletes the file in case the active database needs to be recomputed.
      */
     private void dropTable () {
@@ -90,39 +78,108 @@ public class Main {
      * Queries the database one line at a time.
      */
     private void loadData () {
-        int size = computeSize();
         int count = 0;
 
+
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM database LIMIT ? OFFSET 1;");
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM database LIMIT 1 OFFSET ?;");
             ResultSet results;
 
-            while (count <= size) {
+            String sql = "CREATE TABLE IF NOT EXISTS test ("
+                    + "userID varchar(255), ";
+
+            PreparedStatement preparedStat = connection.prepareStatement("select distinct itemID from database limit 1 offset ?;");
+            ResultSet column;
+
+            //creates 5 columns for 5 items
+            while (count < 150) {
+                preparedStatement.setInt(1, count);
+
+                preparedStat.setInt(1, count);
+                column = preparedStat.executeQuery();
+                count++;
+
+                sql = sql.concat("'" + column.getInt(1) + "'" + " varchar(255), ");
+                // space for any computation
+            }
+            sql = sql.substring(0, sql.length() - 2) + ");";
+            System.out.println(sql);
+
+            statement.execute(sql);
+
+            statement.execute("INSERT INTO test (userID) select distinct userID from database;");
+
+            count = 0;
+            while (count < 150) {
                 preparedStatement.setInt(1, count);
                 results = preparedStatement.executeQuery();
                 count++;
 
-                // space for any computation
+                statement.execute("update test set '" + results.getInt(2) + "' = (select rating from database where" +
+                        " userID = " + results.getInt(1) + " and itemID = " + results.getInt(2) + ") where userID = " +
+                        results.getInt(1) + ";");
             }
+
             System.out.println("Database queried: " + count + " records.");
         } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    /**
-     * Queries and returns the number of rows in a database.
-     * @return the number of rows in the queried database
-     */
-    private int computeSize () {
+    private void computeAvgMap(){
         try {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT COUNT(*) FROM database");
-            ResultSet result = preparedStatement.executeQuery();
-            return result.getInt(1);
+            float f;
+            ResultSet res = connection.createStatement().executeQuery("select userID, cast(avg(rating) as real) from database group by userID;");
+            while (res.next()){
+                f = res.getFloat(2);
+                map.put(res.getInt(1), f);
+            }
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return 0;
+
+    }
+    /**
+     * Calculates average of all ratings of one user.
+     */
+    private float average (int par) {
+        if (map.containsKey(par)) {
+            return map.get(par);
+        }
+        return -1;
+    }
+
+    /**
+     * Gets all users that rated two certain given items.
+     */
+    private float formula (int item1, int item2) {
+        try {
+            stat.setInt(1, item1);
+            stat.setInt(2, item2);
+            ResultSet res = stat.executeQuery();
+
+            float c1, c2;
+            float average;
+            float numarator = -1;
+
+            while (res.next()) {
+                average = average(res.getInt(1));
+
+                c1 = res.getInt(2) - average;
+
+                c2 = res.getInt(3) - average;
+
+                numarator += c1 * c2;
+            }
+            if (numarator == -1)
+                System.out.println("set is empty for " + item1 + " and " + item2);
+
+            return numarator;
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return -1;
     }
 
     /**
@@ -135,9 +192,20 @@ public class Main {
         Main main = new Main();
 
         main.connectDatabase();
-        //main.query();
-        main.loadData();
 
+        main.stat = main.connection.prepareStatement("select s.userID, s.rating, p.rating from database s inner join database p where s.userID = p.userID and s.itemID = ? AND p.itemID = ?;");
+
+        main.computeAvgMap();
+        System.out.println("averages done " + (System.currentTimeMillis() - startTime)/1000);
+
+        for(int i = 1; i < 5000; ++i){
+            for(int j = i+1; j<5000; ++j) {
+                System.out.println("i[" + i + "] j[" + j + "]");
+                long st = System.currentTimeMillis();
+                main.simMatrix[i][j] = main.formula(i, j);
+                System.out.println("checked in: " + (System.currentTimeMillis() - st)/1000);
+            }
+        }
         main.connection.close();
 
         long endTime   = System.currentTimeMillis();
